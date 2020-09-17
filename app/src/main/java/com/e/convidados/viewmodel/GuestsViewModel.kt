1079d4413bc3dd24a19278de345a243baddbfa5b
LuisTaborda <luisgustavo.taborda@gmail.com>
2020-09-17 19:07:26 -0300

    Implementando Room no projeto.

--- app/src/main/java/com/e/convidados/viewmodel/GuestsViewModel.kt
+++ app/src/main/java/com/e/convidados/viewmodel/GuestsViewModel.kt
@@ -11,7 +11,7 @@ import com.e.convidados.service.repository.GuestRepository
 
 class GuestsViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val mGuestRepository = GuestRepository.getInstance(application.applicationContext)
+    private val mGuestRepository = GuestRepository(application.applicationContext)
 
     private val mGuestList = MutableLiveData<List<GuestModel>>()
     val guestList: LiveData<List<GuestModel>> = mGuestList
@@ -26,7 +26,8 @@ class GuestsViewModel(application: Application) : AndroidViewModel(application) 
         }
     }
 
-    fun delete(id: Int): Boolean {
-        return mGuestRepository.delete(id)
+    fun delete(id: Int) {
+        val guest = mGuestRepository.get(id)
+        return mGuestRepository.delete(guest)
     }
 }
